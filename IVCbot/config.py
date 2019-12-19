69cb6d1a9c86bd0900240ea5dd6ee13e4474cdc2
Ilya <stemasoff1@yandex.ru>
2019-12-19 16:44:57 +0300

    Написал бота для мониторинга записи.

--- IVCbot/config.py
+++ IVCbot/config.py
@@ -1,8 +1,8 @@
-TG_TOKEN = '1033673387:AAH2ej2zo1dxbm7fVDXBWzvLuYMVOo-uqro'
+TG_TOKEN = 'Your token'
 #socks_proxy = 'socks5h://110.49.101.58:1080'
-legalUsers = [657637691, 176921961, 159251957]				# id пользователей, с которыми можно общаться
+legalUsers = []				# id пользователей, с которыми можно общаться
 
 pathVideo = '/home/cam/video/'
 #pathScriptRecord = '/home/cam/scripts/nastya/main.py'
-cam1 = {'name': 'cam1', 'ip': '192.168.50.22'}
-cam2 = {'name': 'cam2', 'ip': '192.168.50.23'}
+cam1 = {'name': 'cam1', 'ip': '192.168.xx.xx'}
+cam2 = {'name': 'cam2', 'ip': '192.168.xx.xx'}
